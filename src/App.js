315e30a8ed9e047f1455032b29cca1b4912a6560
meghamalviya5 <megha.malviya444@gmail.com>
2023-05-20 23:42:56 +0530

    feat: added navbar & category listing on home page

--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import logo from "./logo.png";
+
+import Mockman from "mockman-js";
+import NavBar from "./frontend/components/NavBar/NavBar";
+import Home from "./frontend/pages/Home/Home";
+import GiftListing from "./frontend/pages/GiftListing/GiftListing";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/api/category/:categoryId" element={<GiftListing />} />
+          <Route path="/mockman" element={<Mockman />} />
+        </Routes>
       </header>
     </div>
   );
